5029f6427f12c770d348f00597617d240fc17a51
João Lucas Lourenço <jao.lucas.lourenco@gmail.com>
2023-11-02 16:33:20 -0300

    First sprint

---
+++ FrmCadastro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATEX2023_2
+{
+    public partial class FrmCadastro : MetroFramework.Forms.MetroForm
+    {
+        public FrmCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FrmModerador frmModerador = new FrmModerador();
+            this.Hide();
+            frmModerador.ShowDialog();
+            this.Close();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            FrmInicio frmInicio = new FrmInicio();
+            this.Hide();
+            frmInicio.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ FrmInicio.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ATEX2023_2
+{
+    partial class FrmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblInicio = new System.Windows.Forms.Label();
+            this.lblJogos = new System.Windows.Forms.Label();
+            this.lblRedeSocial = new System.Windows.Forms.Label();
+            this.lblSobre = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pctJogos = new System.Windows.Forms.PictureBox();
+            this.pctRedes = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctJogos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRedes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(804, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "__________________________________________________________________";
+            // 
+            // lblInicio
+            // 
+            this.lblInicio.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblInicio.AutoSize = true;
+            this.lblInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInicio.Location = new System.Drawing.Point(105, 48);
+            this.lblInicio.Name = "lblInicio";
+            this.lblInicio.Size = new System.Drawing.Size(85, 26);
+            this.lblInicio.TabIndex = 5;
+            this.lblInicio.Text = "INICIO";
+            this.lblInicio.MouseLeave += new System.EventHandler(this.lblInicio_MouseLeave);
+            this.lblInicio.MouseHover += new System.EventHandler(this.lblInicio_MouseHover);
+            // 
+            // lblJogos
+            // 
+            this.lblJogos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblJogos.AutoSize = true;
+            this.lblJogos.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJogos.Location = new System.Drawing.Point(271, 51);
+            this.lblJogos.Name = "lblJogos";
+            this.lblJogos.Size = new System.Drawing.Size(75, 24);
+            this.lblJogos.TabIndex = 6;
+            this.lblJogos.Text = "JOGOS";
+            this.lblJogos.MouseLeave += new System.EventHandler(this.lblJogos_MouseLeave);
+            this.lblJogos.MouseHover += new System.EventHandler(this.lblJogos_MouseHover);
+            // 
+            // lblRedeSocial
+            // 
+            this.lblRedeSocial.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblRedeSocial.AutoSize = true;
+            this.lblRedeSocial.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRedeSocial.Location = new System.Drawing.Point(427, 51);
+            this.lblRedeSocial.Name = "lblRedeSocial";
+            this.lblRedeSocial.Size = new System.Drawing.Size(134, 24);
+            this.lblRedeSocial.TabIndex = 7;
+            this.lblRedeSocial.Text = "REDE SOCIAL";
+            this.lblRedeSocial.MouseLeave += new System.EventHandler(this.lblRedeSocial_MouseLeave);
+            this.lblRedeSocial.MouseHover += new System.EventHandler(this.lblRedeSocial_MouseHover);
+            // 
+            // lblSobre
+            // 
+            this.lblSobre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSobre.AutoSize = true;
+            this.lblSobre.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSobre.Location = new System.Drawing.Point(642, 51);
+            this.lblSobre.Name = "lblSobre";
+            this.lblSobre.Size = new System.Drawing.Size(189, 24);
+            this.lblSobre.TabIndex = 8;
+            this.lblSobre.Text = "SOBRE O PROJETO";
+            this.lblSobre.MouseLeave += new System.EventHandler(this.lblSobre_MouseLeave);
+            this.lblSobre.MouseHover += new System.EventHandler(this.lblSobre_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(333, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(290, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Onde você passa a maior parte do tempo?";
+            // 
+            // pctJogos
+            // 
+            this.pctJogos.Image = global::ATEX2023_2.Properties.Resources.Jogos;
+            this.pctJogos.Location = new System.Drawing.Point(490, 161);
+            this.pctJogos.Name = "pctJogos";
+            this.pctJogos.Size = new System.Drawing.Size(233, 388);
+            this.pctJogos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctJogos.TabIndex = 10;
+            this.pctJogos.TabStop = false;
+            this.pctJogos.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pctRedes
+            // 
+            this.pctRedes.Image = global::ATEX2023_2.Properties.Resources.RedeSocial;
+            this.pctRedes.Location = new System.Drawing.Point(208, 161);
+            this.pctRedes.Name = "pctRedes";
+            this.pctRedes.Size = new System.Drawing.Size(233, 388);
+            this.pctRedes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctRedes.TabIndex = 9;
+            this.pctRedes.TabStop = false;
+            this.pctRedes.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pctRedes.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pctRedes.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // FrmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 608);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pctJogos);
+            this.Controls.Add(this.pctRedes);
+            this.Controls.Add(this.lblSobre);
+            this.Controls.Add(this.lblRedeSocial);
+            this.Controls.Add(this.lblJogos);
+            this.Controls.Add(this.lblInicio);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmInicio";
+            ((System.ComponentModel.ISupportInitialize)(this.pctJogos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRedes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInicio;
+        private System.Windows.Forms.Label lblJogos;
+        private System.Windows.Forms.Label lblRedeSocial;
+        private System.Windows.Forms.Label lblSobre;
+        private System.Windows.Forms.PictureBox pctRedes;
+        private System.Windows.Forms.PictureBox pctJogos;
+        private System.Windows.Forms.Label label2;
+    }
+}
